fe5e9249c89a8344c6d660ae1a396f0ad7b4f377
Aleksey <example@example.com>
2024-05-13 12:36:09 +0300

    Description of changes

--- tests/test_even_odd.py
+++ tests/test_even_odd.py
@@ -9,7 +9,7 @@ def test_even_odd():
     assert check_even_odd(2) == "Четное число"
     assert check_even_odd(3) == "Нечетное число"
     assert check_even_odd(0) == "Четное число"
-    assert check_even_odd(-2) == "Нечетное число"
+    assert check_even_odd(-1) == "Нечетное число"
     assert check_even_odd(13) == "Нечетное число"
 
 if __name__ == "__main__":
